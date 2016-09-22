725342771574f7f67142f9670c175de3cf2ea9de
vlahm <vlahm13@gmail.com>
2016-09-21 22:01:02 -0700

    duration prints and prompt returns

--- Chime/chime/chime.py
+++ Chime/chime/chime.py
@@ -1,6 +1,9 @@
 from sys import argv
-import pyglet, subprocess, time, re, os, sys
+from time import sleep
+from sys import exit
 from os.path import abspath
+import pyglet, re, subprocess
+import threading
 
 bellpath = abspath("/home/mike/func_tools/bicycle_bell.wav")
 bell = pyglet.media.load(bellpath)
@@ -16,7 +19,7 @@ hms = [None]*3
 # parse duration string and convert to seconds
 x = re.search(r'[hms]', dur)
 if x is None:
-    sys.exit("Supply hours, mins, and/or secs in the form 'xhxmxs', where "
+    exit("Supply hours, mins, and/or secs in the form 'xhxmxs', where "
     "each 'x' is numeric. No quotes needed.")
 
 hms[0] = re.match(r'((\d+)h)?', dur).group(2)
@@ -26,26 +29,33 @@ hms[2] = re.match(r'(?:\d+h)?(?:\d+m)?((\d+)s)?', dur).group(2)
 hms = [0 if x is None else int(x) for x in hms]
 
 if hms == [0,0,0]:
-    sys.exit("Supply hours,w mins, and/or secs in the form 'xhxmxs', where "
+    exit("Supply hours,w mins, and/or secs in the form 'xhxmxs', where "
     "each 'x' is numeric. No quotes needed.")
 
-print('Timing for %s hour(s), %s minute(s), and %s second(s).' % tuple(hms))
+def print_message():
+    print('Timing for %s hour(s), %s minute(s), and %s second(s).' % tuple(hms))
 
-wait_time = 3600*hms[0] + 60*hms[1] + hms[2]
-time.sleep(wait_time)
+def timer():
+    wait_time = 3600*hms[0] + 60*hms[1] + hms[2]
+    sleep(wait_time)
 
-# open a new terminal window and print reminder 
-# 'exec bash' keeps the window open
-gnome_command = "bash -c \"echo -e 'Ding!\\n%s'; exec bash\"" % reminder
-subprocess.call(['gnome-terminal', '-e', gnome_command])
+    # open a new terminal window and print reminder 
+    # 'exec bash' keeps the window open
+    gnome_command = "bash -c \"echo -e 'Ding!\\n%s'; exec bash\"" % reminder
+    subprocess.call(['gnome-terminal', '-e', gnome_command])
+    
+    # remove dumpfile
+    dumpfile = abspath("/home/mike/git/funcs/Chime/chime/nohup.out")
+    #try:
+    #    os.remove(dumpfile)
+    #except OSError:
+    #    pass
+    
+    bell.play()
+    sleep(0.7)
 
-# remove dumpfile
-dumpfile = abspath("/home/mike/git/funcs/Chime/chime/nohup.out")
-#try:
-#    os.remove(dumpfile)
-#except OSError:
-#    pass
-
-bell.play()
-time.sleep(0.7)
-
+t1 = threading.Thread(target=timer)
+t2 = threading.Thread(target=print_message)
+#t1.setDaemon(True)
+t1.start()
+t2.start()
